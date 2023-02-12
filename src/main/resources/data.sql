a36ac86158376b7ce49426d392663f46bb0ec26f
mostafaadel <mostafa.adel90@gmail.com>
2023-02-13 01:54:17 +0200

    update data.sql with UUIDs for drone's serial numbers

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,19 @@
 INSERT INTO Drone (serial_Number, model, state, weight_Limit_In_Grams, remaining_Weight_In_Grams,
                    battery_Capacity_Percentage)
-VALUES ('PreGeneratedDroneSerialNumber1', 'LIGHTWEIGHT', 'IDLE', 100, 100, 50);
+VALUES ('84cf7428-3169-4298-90ec-2a4a63314c0a', 'LIGHTWEIGHT', 'IDLE', 100, 100, 50);
 
 INSERT INTO Drone (serial_Number, model, state, weight_Limit_In_Grams, remaining_Weight_In_Grams,
                    battery_Capacity_Percentage)
-VALUES ('PreGeneratedDroneSerialNumber2', 'LIGHTWEIGHT', 'IDLE', 200, 200, 60);
+VALUES ('4daeb20d-ce62-472b-81ea-e4f13e50b0ee', 'LIGHTWEIGHT', 'IDLE', 200, 200, 60);
 
 INSERT INTO Drone (serial_Number, model, state, weight_Limit_In_Grams, remaining_Weight_In_Grams,
                    battery_Capacity_Percentage)
-VALUES ('PreGeneratedDroneSerialNumber3', 'MIDDLEWEIGHT', 'IDLE', 300, 300, 70);
+VALUES ('cc089ffe-ea19-468d-bbf9-66b2cdca1186', 'MIDDLEWEIGHT', 'IDLE', 300, 300, 70);
 
 INSERT INTO Drone (serial_Number, model, state, weight_Limit_In_Grams, remaining_Weight_In_Grams,
                    battery_Capacity_Percentage)
-VALUES ('PreGeneratedDroneSerialNumber4', 'CRUISERWEIGHT', 'IDLE', 400, 400, 80);
+VALUES ('8745edb6-5096-448d-88f1-aad7ac1df7df', 'CRUISERWEIGHT', 'IDLE', 400, 400, 80);
 
 INSERT INTO Drone (serial_Number, model, state, weight_Limit_In_Grams, remaining_Weight_In_Grams,
                    battery_Capacity_Percentage)
-VALUES ('PreGeneratedDroneSerialNumber5', 'HEAVYWEIGHT', 'IDLE', 500, 500, 90);
+VALUES ('e45b9203-8736-406f-a74c-0142edec407a', 'HEAVYWEIGHT', 'IDLE', 500, 500, 90);
